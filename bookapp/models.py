ec9f40713c74aca7e16c8b57365de5a1bd8c6a9b
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-22 11:51:01 +0530

    Add models for book app
    Add models and modelfields for book app

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text="Enter book title")
+    isbn = models.UUIDField(unique=True)
+    no_of_page = models.DecimalField("Enter no.of page",decimal_places=0,max_digits=6)
+    cover_image = models.ImageField()
+    description = models.TextField(max_length=500, help_text="Enter description")
+    genre = models.ManyToManyField('Genre')
+    publisher = models.ManyToManyField('Publisher')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True, blank=True)
+    published_date = models.DateField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter genre name")
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter genre name")
+    address = models.CharField(max_length=500, help_text="Enter address")
+    description = models.TextField(max_length=500, help_text="Enter description")
+    phone = models.DecimalField(decimal_places=0, max_digits=10)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the name")
+    bio = models.TextField(max_length=500, help_text="Enter bio")
+    phone = models.DecimalField(decimal_places=0, max_digits=10)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
