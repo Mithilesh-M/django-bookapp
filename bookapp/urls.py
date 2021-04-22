f77f6a3e0f6d58e6d504c1c8d854962d4f440132
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-22 14:43:58 +0530

    Add book list view in book app
    Add book list in book app

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book/',views.Booklistview.as_view(), name='book-list'),
 ]
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Genre, Publisher, Author
+from django.views import generic
 
 def index(request):
     context = {
@@ -9,3 +10,7 @@ def index(request):
         'author': Author.objects.all().count(),
     }
     return render(request, 'bookapp/index.html', context)
+
+
+class Booklistview(generic.ListView):
+    model = Book
