79da9e87a4bc210b8c7cb3d33d48e1ce11d44070
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-22 17:16:58 +0530

    Update model in book app
    Modified field for phone number in author and publisher

--- bookapp/models.py
+++ bookapp/models.py
@@ -29,7 +29,7 @@ class Publisher(models.Model):
     name = models.CharField(max_length=200, help_text="Enter genre name")
     address = models.CharField(max_length=500, help_text="Enter address")
     description = models.TextField(max_length=500, help_text="Enter description")
-    phone = models.DecimalField(decimal_places=0, max_digits=10)
+    phone = models.CharField(max_length=12, help_text="Enter phone number")
 
     def __str__(self):
         """String for representing the Model object."""
@@ -39,7 +39,7 @@ class Publisher(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=200, help_text="Enter the name")
     bio = models.TextField(max_length=500, help_text="Enter bio")
-    phone = models.DecimalField(decimal_places=0, max_digits=10)
+    phone = models.CharField(max_length=12, help_text="Enter phone number")
 
     def __str__(self):
         """String for representing the Model object."""
